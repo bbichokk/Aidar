af001e0688b0a67200544c6d87207bb33772a23f
Aidar <59378420+bbichokk@users.noreply.github.com>
2022-01-26 15:19:42 +0100

    cats change

--- src/equipo/ejercicio1.java
+++ src/equipo/ejercicio1.java
@@ -4,25 +4,25 @@ public class ejercicio1 {
 
 	public static void main(String[] args) {
 		
-		int gatos=20;
+		int cats=20;
 		int ratones=3500;
 		int dias=0;
 		
 		do {
-			ratones = ratones - gatos;
+			ratones = ratones - cats;
 			dias++;
 			
 			
 			if (dias == 200 || dias == 400)
 			{
-				gatos = gatos*4;
+				cats = cats*4;
 			}
 			
 			if(dias == 100 || dias == 200 || dias == 300 || dias == 400 || dias == 500)
 			{
 				System.out.println("Han pasado "+ dias +"dias");
 				System.out.println("Hay "+ ratones +"ratones");
-				System.out.println("Hay "+ gatos +"gatos");
+				System.out.println("Hay "+ cats +"gatos");
 				
 				ratones = ratones*2;
 			}	
